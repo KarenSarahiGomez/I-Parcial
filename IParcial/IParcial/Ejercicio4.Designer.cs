fd17a427a80fa713ac26bf4b00064b9c2e0a7996
Karen Gómez <ksgomezg@unah.hn>
2023-02-03 17:04:18 -0600

    Ejercicio4
    Fecha, meses y años

---
+++ IParcial/IParcial/Ejercicio4.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace IParcial
+{
+    partial class Ejercicio4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FechadateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.Ejecutarbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DiatextBox = new System.Windows.Forms.TextBox();
+            this.MestextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AniotextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SemanatextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DiastextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CitatextBox = new System.Windows.Forms.TextBox();
+            this.DiasRestatextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.NuevaFechatextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // FechadateTimePicker
+            // 
+            this.FechadateTimePicker.Location = new System.Drawing.Point(119, 22);
+            this.FechadateTimePicker.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.FechadateTimePicker.Name = "FechadateTimePicker";
+            this.FechadateTimePicker.Size = new System.Drawing.Size(407, 26);
+            this.FechadateTimePicker.TabIndex = 0;
+            // 
+            // Ejecutarbutton
+            // 
+            this.Ejecutarbutton.Location = new System.Drawing.Point(384, 82);
+            this.Ejecutarbutton.Name = "Ejecutarbutton";
+            this.Ejecutarbutton.Size = new System.Drawing.Size(142, 39);
+            this.Ejecutarbutton.TabIndex = 1;
+            this.Ejecutarbutton.Text = "Ejecutar";
+            this.Ejecutarbutton.UseVisualStyleBackColor = true;
+            this.Ejecutarbutton.Click += new System.EventHandler(this.Ejecutarbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Días: ";
+            // 
+            // DiatextBox
+            // 
+            this.DiatextBox.Location = new System.Drawing.Point(203, 170);
+            this.DiatextBox.Name = "DiatextBox";
+            this.DiatextBox.Size = new System.Drawing.Size(156, 26);
+            this.DiatextBox.TabIndex = 3;
+            // 
+            // MestextBox
+            // 
+            this.MestextBox.Location = new System.Drawing.Point(203, 223);
+            this.MestextBox.Name = "MestextBox";
+            this.MestextBox.Size = new System.Drawing.Size(156, 26);
+            this.MestextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mes: ";
+            // 
+            // AniotextBox
+            // 
+            this.AniotextBox.Location = new System.Drawing.Point(203, 275);
+            this.AniotextBox.Name = "AniotextBox";
+            this.AniotextBox.Size = new System.Drawing.Size(156, 26);
+            this.AniotextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(119, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Año: ";
+            // 
+            // SemanatextBox
+            // 
+            this.SemanatextBox.Location = new System.Drawing.Point(203, 323);
+            this.SemanatextBox.Name = "SemanatextBox";
+            this.SemanatextBox.Size = new System.Drawing.Size(156, 26);
+            this.SemanatextBox.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Semana: ";
+            // 
+            // DiastextBox
+            // 
+            this.DiastextBox.Location = new System.Drawing.Point(203, 372);
+            this.DiastextBox.Name = "DiastextBox";
+            this.DiastextBox.Size = new System.Drawing.Size(156, 26);
+            this.DiastextBox.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(119, 373);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Cita:";
+            // 
+            // CitatextBox
+            // 
+            this.CitatextBox.Location = new System.Drawing.Point(384, 373);
+            this.CitatextBox.Name = "CitatextBox";
+            this.CitatextBox.Size = new System.Drawing.Size(272, 26);
+            this.CitatextBox.TabIndex = 12;
+            // 
+            // DiasRestatextBox
+            // 
+            this.DiasRestatextBox.Location = new System.Drawing.Point(203, 420);
+            this.DiasRestatextBox.Name = "DiasRestatextBox";
+            this.DiasRestatextBox.Size = new System.Drawing.Size(156, 26);
+            this.DiasRestatextBox.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(119, 421);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Restar:";
+            // 
+            // NuevaFechatextBox
+            // 
+            this.NuevaFechatextBox.Location = new System.Drawing.Point(384, 421);
+            this.NuevaFechatextBox.Name = "NuevaFechatextBox";
+            this.NuevaFechatextBox.Size = new System.Drawing.Size(272, 26);
+            this.NuevaFechatextBox.TabIndex = 15;
+            // 
+            // Ejercicio4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 473);
+            this.Controls.Add(this.NuevaFechatextBox);
+            this.Controls.Add(this.DiasRestatextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.CitatextBox);
+            this.Controls.Add(this.DiastextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.SemanatextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.AniotextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MestextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DiatextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ejecutarbutton);
+            this.Controls.Add(this.FechadateTimePicker);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Name = "Ejercicio4";
+            this.Text = "Ejercicio4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker FechadateTimePicker;
+        private System.Windows.Forms.Button Ejecutarbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox DiatextBox;
+        private System.Windows.Forms.TextBox MestextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AniotextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox SemanatextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox DiastextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox CitatextBox;
+        private System.Windows.Forms.TextBox DiasRestatextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox NuevaFechatextBox;
+    }
+}
---
+++ IParcial/IParcial/Ejercicio4.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IParcial
+{
+    public partial class Ejercicio4 : Form
+    {
+        public Ejercicio4()
+        {
+            InitializeComponent();
+        }
+
+        private void Ejecutarbutton_Click(object sender, EventArgs e)
+        {
+            DateTime fecha = FechadateTimePicker.Value;
+
+            DiatextBox.Text = fecha.Day.ToString();
+            MestextBox.Text = fecha.ToString("MMMM");
+            AniotextBox.Text = fecha.Year.ToString();    
+            SemanatextBox.Text = fecha.ToString("dddd");
+            //Decimal numero = 0;
+            //numero.ToString("M");
+
+            int numeroDias = Convert.ToInt32(DiastextBox.Text);
+            DateTime fechaActual = DateTime.Now;
+
+            CitatextBox.Text = fechaActual.AddDays(numeroDias).ToShortDateString();
+            int diasresta = Convert.ToInt32(DiastextBox.Text);
+            NuevaFechatextBox.Text = fecha.AddDays(-diasresta).ToLongDateString();
+
+            //Esto se hizo despues del private int...
+            MessageBox.Show("La edad es: " + DevolverEdad(fecha));
+            DevolverEdad(fecha);
+        }
+        private int DevolverEdad(DateTime fechaNacimiento)
+        {
+            DateTime fechaActual = DateTime.Now;
+            int edad = 0;
+
+            if (fechaNacimiento  >= fechaActual)
+                    {
+                     return 0;
+                    }
+            else
+            {
+                edad = fechaActual.Year - fechaNacimiento.Year;
+                if (fechaNacimiento.Month > fechaActual.Month)
+                {
+                    --edad;
+                    // edad = edad - 1;
+                    //edad - = 1;
+                }
+                return edad;
+            }
+          
+        }
+    }
+}

ff721b21854013b2a53a4cb348a41265c74c81cd
Karen Gómez <ksgomezg@unah.hn>
2023-02-09 16:07:55 -0600

    Ejercicio7
    Filas y Columnas

---
+++ IParcial/IParcial/Ejercicio7.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace IParcial
+{
+    partial class Ejercicio7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.filatextBox = new System.Windows.Forms.TextBox();
+            this.columnatextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(41, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(152, 412);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tamaño Matriz: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(364, 182);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // filatextBox
+            // 
+            this.filatextBox.Location = new System.Drawing.Point(239, 19);
+            this.filatextBox.Name = "filatextBox";
+            this.filatextBox.Size = new System.Drawing.Size(100, 25);
+            this.filatextBox.TabIndex = 4;
+            // 
+            // columnatextBox
+            // 
+            this.columnatextBox.Location = new System.Drawing.Point(239, 59);
+            this.columnatextBox.Name = "columnatextBox";
+            this.columnatextBox.Size = new System.Drawing.Size(100, 25);
+            this.columnatextBox.TabIndex = 5;
+            // 
+            // Ejercicio7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 453);
+            this.Controls.Add(this.columnatextBox);
+            this.Controls.Add(this.filatextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Ejercicio7";
+            this.Text = "Ejercicio7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox filatextBox;
+        private System.Windows.Forms.TextBox columnatextBox;
+    }
+}
---
+++ IParcial/IParcial/Ejercicio7.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace IParcial
+{
+    public partial class Ejercicio7 : Form
+    {
+        public Ejercicio7()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); 
+            //Matriz [3 y 3 es decir 3 filas y 3 columnas]
+            int[,] matriz1 = new int[3, 3] {
+                { 3, 6, 3 },
+                { 6, 9, 7 },
+                { 8, 9, 4 }
+                                           };
+            Random random = new Random();
+
+            int valorFila = Convert.ToInt32(filatextBox.Text); 
+            int valorColumna = Convert.ToInt32(columnatextBox.Text);
+            //Next genera valores aleatorios internos 100, 2000, etc.
+
+
+            int[,] matriz2 = new int[valorFila, valorColumna]; 
+            //get lenhgt para aumentar el tamaño de la mtriz
+
+            //Llenando la matriz 
+            for (int fila = 0; fila < matriz2.GetLength(0); fila++)
+            {
+                for (int columna = 0; columna < matriz2.GetLength(1); columna++)
+                {
+                    matriz2[fila, columna] = random.Next(1, 100); //(en la posc.)
+                }
+            }
+
+            //Mostrando los valores de la matriz en el textBox
+            for (int fila = 0; fila < matriz2.GetLength(0); fila++)
+            {
+                for (int columna = 0; columna < matriz2.GetLength(1); columna++)
+                {
+                    listBox1.Items.Add("La posición: ["+fila+","+columna+"] = " + matriz2[fila, columna]);
+                }
+            }
+
+            textBox1.Text = matriz2.GetLength(0).ToString() + "x" + matriz2.GetLength(1).ToString();
+        }
+    }
+}

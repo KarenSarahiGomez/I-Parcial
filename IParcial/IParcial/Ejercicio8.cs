c87a3e25c367c54b2c6634a254c26157f6745788
Karen Gómez <ksgomezg@unah.hn>
2023-02-09 16:18:21 -0600

    Ejercicio8
    Recursividad

---
+++ IParcial/IParcial/Ejercicio8.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IParcial
+{
+    public partial class Ejercicio8 : Form
+    {
+        public Ejercicio8()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RecorreDeUnoEnUno(1, 100);
+        } 
+
+        //Función recursiva
+        private void RecorreDeUnoEnUno(int valInicial, int valFinal)
+        {
+            listBox1.Items.Add(valInicial);
+            if (valInicial < valFinal)
+            {
+                RecorreDeUnoEnUno(valInicial + 1, valFinal);
+            }
+        }
+    }
+}
